c0a78f578bda64739d6334c2331da3209d47a135
arminegilavya04 <arminegilavyan04.com>
2024-10-09 23:12:28 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class Recursion
+{
+    static int minIndex(int[] a, int i, int j)
+    {
+        if (i == j)
+            return i;
+
+        int k = minIndex(a, i + 1, j);
+
+        if (a[i] < a[k])
+        {
+            return i;
+        }
+        else
+        {
+            return k;
+        }
+    }
+
+    static void recurSelectionSort(int[] a, int n, int index)
+    {
+        if (index == n)
+            return;
+
+        int k = minIndex(a, index, n - 1);
+
+        if (k != index)
+        {
+            int temp = a[k];
+            a[k] = a[index];
+            a[index] = temp;
+        }
+
+        recurSelectionSort(a, n, index + 1);
+    }
+
+    static void PrintArray(int[] a)
+    {
+        for (int i = 0; i < a.Length; i++)
+        {
+            Console.Write(a[i] + " ");
+        }
+        Console.Write("\n");  
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] arr = { 3, 1, 5, 2, 7, 0 };
+
+        Console.Write("Array before sorting:\n"); 
+        PrintArray(arr);
+
+        recurSelectionSort(arr, arr.Length, 0);
+
+        Console.Write("Array after sorting:\n"); 
+        PrintArray(arr);
+    }
+}
